a6aeeee191d6c41b1cc0805ff10ae61464738753
lucasdeabreu <lucasdeabreu@gmail.com>
2020-06-14 22:16:24 -0300

    Select chapter

---
+++ select/racer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a string, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		fmt.Printf("Finish Get %s\n", url)
+		close(ch)
+	}()
+	return ch
+}
